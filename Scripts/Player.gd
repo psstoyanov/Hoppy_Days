1befeced3ce4cba70192301544dc94f748a985dc
PakoSt <kokotekilata@gmail.com>
2018-11-27 20:29:44 +0000

    S3L22 - Jumping
    - Add simplistic Gravity to the main character
    - Link the Player Scene to a Level Scene

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,19 @@
 extends KinematicBody2D
 
 const SPEED = 600
+const GRAVITY = 5000
+const UP = Vector2(0,-1)
+const JUMP_SPEED = -1500
 
 var motion = Vector2()
 
 func _physics_process(delta):
-	
+	fall(delta)
+	run()
+	jump()
+	move_and_slide(motion, UP)
+
+func run():
 	if Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
 		motion.x = SPEED
 		$AnimatedSprite.play("run")
@@ -19,5 +27,13 @@ func _physics_process(delta):
 		motion.x = 0
 		$AnimatedSprite.play("idle")
 		$AnimatedSprite.flip_h = false
-		
-	move_and_slide(motion)
+
+func fall(delta):
+	if is_on_floor():
+		motion.y = 0
+	else:
+		motion.y += GRAVITY * delta
+
+func jump():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		motion.y = JUMP_SPEED
