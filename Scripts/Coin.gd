06fc362ecdb078a8fe9176138a74f41b4a5dc419
PakoSt <kokotekilata@gmail.com>
2018-11-28 14:17:47 +0000

    S3L39 - Coin pickup bug
    - Coins no longer can be picked up multiple times

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -1,11 +1,14 @@
 extends AnimatedSprite
 
+var taken = false
 
 
 func _on_body_entered(body):
-	Global.GameState.coin_up()
-	$CoinSFX.play()
-	$AnimationPlayer.play("die")
+	if not taken:
+		Global.GameState.coin_up()
+		$CoinSFX.play()
+		$AnimationPlayer.play("die")
+		taken = true
 
 func die():
 	queue_free()
